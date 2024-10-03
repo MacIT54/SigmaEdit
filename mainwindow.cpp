e6fee5ae00627d34354f6afab9cd5ec7fe929827
Mikhail_Mrikh <158068110+Mikhail-Mrikh@users.noreply.github.com>
2024-10-03 21:14:30 +0700

    OpenFile

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QTextStream>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    textEdit(new QTextEdit(this)),
+    openButton(new QPushButton("Открыть файл", this)) // Создаем кнопку
 {
     ui->setupUi(this);
+
+    // Устанавливаем основной компоновщик
+    QVBoxLayout *layout = new QVBoxLayout; // Компоновщик вертикального расположения
+    layout->addWidget(openButton); // Добавляем кнопку в компоновщик
+    layout->addWidget(textEdit); // Добавляем текстовый редактор в компоновщик
+
+    // Создаем виджет и устанавливаем компоновщик
+    QWidget *centralWidget = new QWidget(this);
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget);
+
+    // Скрываем текстовый редактор
+    textEdit->setVisible(false);
+
+    // Подключаем сигнал кнопки к слоту
+    connect(openButton, &QPushButton::clicked, this, &MainWindow::openFile);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::openFile() {
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Text Files (*.txt);;All Files (*)"));
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream in(&file);
+            textEdit->setPlainText(in.readAll());
+            file.close();
+        }
+        // Показываем текстовый редактор после открытия файла
+        textEdit->setVisible(true);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTextEdit>
+#include <QTableWidget>
+#include <QPushButton>
 
-namespace Ui {
-class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void openFile();
 
 private:
     Ui::MainWindow *ui;
+    QTextEdit *textEdit; // текстовый редактор
+    QPushButton *openButton; // кнопка "Открыть файл"
 };
 
 #endif // MAINWINDOW_H
